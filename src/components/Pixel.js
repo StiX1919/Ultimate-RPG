4be2a681f20b63736bb95c349106ea7927d8eec2
Spencer Smith <stix1919@hotmail.com>
2018-08-25 20:46:06 -0500

    some notes

--- src/components/Pixel.js
+++ src/components/Pixel.js
@@ -7,7 +7,7 @@ class Pixel extends Component {
         super(props)
 
     }
-
+//border exists only if size is 10px
     render(){
         return (
             <div style={{border: this.props.pixSize === 10 ? 'solid black .5px' : 'solid black 0',width: this.props.pixSize + 'px', height: this.props.pixSize + 'px', backgroundColor: this.props.pixel.color}}
