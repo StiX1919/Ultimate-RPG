dfbca61adc6f597dfb7511484aae66540ec395b1
Spencer Smith <stix1919@hotmail.com>
2018-08-27 16:34:59 -0500

    a new way to build out pixel array

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ class App extends Component {
       width: 10,
       pixSize: 10,
 
-      pixelArr: []
+      pixelArr: [],
+      newHeight: 10,
+      newWidth: 10
     }
     this.handleColorChange=this.handleColorChange.bind(this)
     this.modifyPixels = this.modifyPixels.bind(this)
@@ -27,6 +29,8 @@ class App extends Component {
     this.handleChangeWidth=this.handleChangeWidth.bind(this)
     this.handlePixSize=this.handlePixSize.bind(this)
 
+    this.updateTable=this.updateTable.bind(this)
+
   }
 
   componentDidMount(){
@@ -34,13 +38,38 @@ class App extends Component {
   }
   
   modifyPixels(){
+    console.log('hello')
+    let oldArr = this.state.pixelArr.slice()
     let pixelArr = []
     
-        for(let c = 0; c < this.state.width; c++){
-          for(let r = 0; r < this.state.height; r++){
-            pixelArr.push({cInd: c, rInd: r, color: '#FFFFFF'})
+        
+        if(oldArr[0]){
+          for(let i = 0, c = 0, r = 0; i < this.state.width*this.state.height; i++){
+            let matchArr = oldArr.filter(item => (item.cInd === c && item.rInd === r))
+            if(c < this.state.width){
+              if(matchArr[0] && matchArr[0].color !== '#FFFFFF'){
+                console.log(matchArr, 'matched')
+                pixelArr.push(Object.assign({}, matchArr[0], {cInd: c, rInd: r}))
+                c++
+              } else {
+                pixelArr.push({cInd: c, rInd: r, color: '#FFFFFF'})
+                c++
+              }
+            } else {
+              c = 0
+              r ++
+              i--
+            }
+
           }
         }
+        else 
+          for(let c = 0; c < this.state.width; c++){
+            for(let r = 0; r < this.state.height; r++){
+              pixelArr.push({cInd: c, rInd: r, color: '#FFFFFF'})
+            }
+          }
+        console.log(oldArr, pixelArr)
     
         this.setState({pixelArr})
   }
@@ -52,19 +81,15 @@ class App extends Component {
 
   async handleChangeHeight(value){
     if(Number(value) > 50){
-      await this.setState({height: 50})
-      this.modifyPixels()
+      await this.setState({newHeight: 50})
     } else
-      await this.setState({ height: Number(value) })
-      this.modifyPixels()
+      await this.setState({ newHeight: Number(value) })
   }
   async handleChangeWidth(value){
     if(Number(value) > 50){
-      await this.setState({width: 50})
-      this.modifyPixels()
+      await this.setState({newWidth: 50})
     } else
-      await this.setState({ width: Number(value) })
-      this.modifyPixels()
+      await this.setState({ newWidth: Number(value) })
   }
 
 
@@ -72,6 +97,7 @@ class App extends Component {
     let newArr = arr
 
     newArr[index].color = color
+    console.log(newArr[index])
     this.setState({pixelArr: newArr})
   }
 
@@ -82,10 +108,19 @@ class App extends Component {
     this.setState({pixSize: Number(value)})
   }
 
+
+  async updateTable(event){
+    console.log(event.key)
+    if(event.key === 'Enter'){
+      await this.setState({width: this.state.newWidth, height: this.state.newHeight})
+      this.modifyPixels()
+
+    }
+  }
   
 
   render() {
-    // console.log(this.state)
+    console.log(this.state)
 
     return (
       <div className="App">
@@ -112,11 +147,10 @@ class App extends Component {
           </div>
 
           <div>
-          <h3>Height:</h3><input type='number' value={this.state.height} placeholder={this.state.height} onChange={(e) => this.handleChangeHeight(e.target.value)}/>
-          <h3>Width:</h3><input type='number' value={this.state.width} placeholder={this.state.width} onChange={(e) => this.handleChangeWidth(e.target.value)}/>
-          <h3>Pixel Size:</h3><input type='number' value={this.state.pixSize} placeholder={10} onChange={(e) => this.handlePixSize(e.target.value)}/>
-        
-        </div>
+            <h3>Height:</h3><input type='number' value={this.state.newHeight} placeholder={this.state.height} onChange={(e) => this.handleChangeHeight(e.target.value)} onKeyDown={this.updateTable}/>
+            <h3>Width:</h3><input type='number' value={this.state.newWidth} placeholder={this.state.width} onChange={(e) => this.handleChangeWidth(e.target.value)} onKeyDown={this.updateTable}/>
+            <h3>Pixel Size:</h3><input type='number' value={this.state.pixSize} placeholder={10} onChange={(e) => this.handlePixSize(e.target.value)}/>
+          </div>
         </div>
         
         
