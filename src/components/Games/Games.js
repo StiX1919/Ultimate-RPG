cc0994c50541f6c62f2e4fe88f43ba87de19dde1
Spencer Smith <stix1919@hotmail.com>
2018-10-22 16:22:33 -0500

    not looking good for box animation

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import {Link} from 'react-router-dom'
-
+import './Games.css'
 class Games extends Component {
     constructor(){
         super()
@@ -20,6 +20,18 @@ class Games extends Component {
       <div>
         <h1>Games</h1>
         {navLinks}
+        {/* animation idea*/}
+        <div className='rotate-square'>
+            <div className='square-top'>
+                <div className='top-1'/>
+                <div className='top-2'/>
+            </div>
+            <div className='square-front-left'>
+                <div className='left-1'/>
+                <div className='left-2'/>
+            </div>
+            <div className='square-front-right'/>
+        </div>
       </div>
     );
   }
