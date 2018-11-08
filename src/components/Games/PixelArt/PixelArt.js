b42a859071577a710602ab20e477ef7d0f0a3f61
Spencer Smith <stix1919@hotmail.com>
2018-11-08 09:51:01 -0600

    cleaning up

--- src/components/Games/PixelArt/PixelArt.js
+++ src/components/Games/PixelArt/PixelArt.js
@@ -65,9 +65,6 @@ class PixelArt extends Component {
       await domtoimage.toPng(node)
          .then(function (dataUrl) {
             image = dataUrl
-            //  var img = new Image();
-            //  img.src = dataUrl;
-            //  document.body.appendChild(img);
          })
          .catch(function (error) {
              console.error('oops, something went wrong!', error);
