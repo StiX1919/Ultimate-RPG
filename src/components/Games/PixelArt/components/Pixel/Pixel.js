e47947b45ce70a4b5ae0b5baaa24d0ae3dd8f680
Spencer Smith <stix1919@hotmail.com>
2018-11-16 11:28:11 -0600

    tried out styled components on pixel. Affects render time so inline it stays..

--- src/components/Games/PixelArt/components/Pixel/Pixel.js
+++ src/components/Games/PixelArt/components/Pixel/Pixel.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import styled from 'styled-components'
 
 // import HeightBox from './HeightBox'
 
@@ -9,7 +10,7 @@ class Pixel extends Component {
             <div style={{border: this.props.border ? 'solid black .5px' : 'solid black 0' ,width: this.props.pixSize + 'px', height: this.props.pixSize + 'px', backgroundColor: this.props.pixel.color, opacity: this.props.pixel.opacity}}
             onClick={() => this.props.chooseColor(this.props.color, this.props.opacity, this.props.index, this.props.allArr)}
             onMouseEnter={e => {
-                if(e.buttons === 1){
+                if(e.buttons === 1 && this.props.color !== this.props.pixel.color){
                     this.props.chooseColor(this.props.color, this.props.opacity, this.props.index, this.props.allArr)
                 }
             }}
