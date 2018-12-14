05985d399ef123c68b560ed039a4aa7de889c591
Spencer Smith <stix1919@hotmail.com>
2018-12-14 14:17:43 -0600

    starting speed based combat

--- src/components/Games/UltimateRPG/components/AdventureScreen/AScomponents/AtkInterface/AtkInterface.js
+++ src/components/Games/UltimateRPG/components/AdventureScreen/AScomponents/AtkInterface/AtkInterface.js
@@ -17,14 +17,23 @@ class AtkInterface extends Component {
         damageDone: 0,
         monDamage: 0,
         attacking: false,
-        monAttacking: false
+        monAttacking: false,
+        heroSpeed: 0,
+        monsterSpeed: 0
     }
-    this.attack = this.attack.bind(this)
+    this.heroAttack = this.heroAttack.bind(this)
+    this.monsterAttack = this.monsterAttack.bind(this)
+
     this.showDamageDone = this.showDamageDone.bind(this)
     this.showMonDamage = this.showMonDamage.bind(this)
 
     this.action = this.action.bind(this)
   }
+  componentDidMount(){
+    this.setState({heroSpeed: this.props.currentHero.speed, monsterSpeed: this.props.currentMonster.spd})
+  }
+
+
   showDamageDone(){
     this.setState({attacking: true})
     setTimeout(() => this.setState({attacking: false}), 800)
@@ -35,7 +44,7 @@ class AtkInterface extends Component {
   }
 
   action(hero, monster){
-        this.attack(hero, monster)
+        this.heroAttack(hero, monster)
     
         setTimeout(() => {
             if(monster.hp - this.state.damageDone > 0){
@@ -58,7 +67,7 @@ class AtkInterface extends Component {
     
   }
 
-  attack(hero, monster){
+  heroAttack(hero, monster){
     let luckFactor = hero.luck/2,
     crit = false,
     critNum = Math.floor(Math.random() * 100) + 1;
@@ -96,48 +105,66 @@ class AtkInterface extends Component {
     let newMon = Object.assign({}, monster, {hp: newHP})
     
     console.log(newMon.hp, 'first')
-    this.setState({damageDone: damage})
+    this.setState({damageDone: damage, monsterSpeed: (monster.spd + this.state.monsterSpeed)})
     this.showDamageDone()
     this.props.attack(newMon)
   }
 
+  monsterAttack(hero, monster){
+    let damage = monster.str - hero.endurance
+    let newHP = hero.hero_hp - damage
+    if(damage < 0){
+        damage = 0
+    }
+    if(newHP >= hero.hero_hp){
+        newHP = hero.hero_hp
+    }
+
+    let newHero = Object.assign({}, hero, {hero_hp: newHP})
+    
+    this.setState({monDamage: damage, heroSpeed: (hero.speed + this.state.heroSpeed)})
+    this.showMonDamage()
+    this.props.hurt(newHero)
+  }
+
 
   render() {
+      console.log(this.state)
         return (
-        <div className='attacks'>
-            {this.state.attacking
-                ? <button disabled >Attack!</button>
-                : <button onClick={() => this.action(this.props.currentHero, this.props.currentMonster)}>Attack!</button>
-            }
-    
-            <div className='damage-dealt'>
-                <div>
-                    <h2>Hero damage</h2>
-                    {this.state.attacking ?
-                        <h1>{this.state.damageDone}</h1>
-                        :
-                        null
-                    
-                    }
+            <div className='attacks'>
+                {this.state.heroSpeed >= this.state.monsterSpeed
+                    ? <button onClick={this.monsterAttack(this.props.currentHero, this.props.currentMonster)}>Attacked!</button>
+                    : <button onClick={() => this.heroAttack(this.props.currentHero, this.props.currentMonster)}>Attack</button>
+                }
+        
+                <div className='damage-dealt'>
+                    <div>
+                        <h2>Hero damage</h2>
+                        {this.state.attacking ?
+                            <h1>{this.state.damageDone}</h1>
+                            :
+                            null
+                        
+                        }
+                    </div>
+                    <div>
+                        <h2>Monster Damage</h2>
+                        {this.state.monAttacking ?
+                            <h1 style={{color: 'red'}}>{this.state.monDamage}</h1>
+                            :
+                            null
+                        
+                        }
+                    </div>
                 </div>
-                <div>
-                    <h2>Monster Damage</h2>
-                    {this.state.monAttacking ?
-                        <h1 style={{color: 'red'}}>{this.state.monDamage}</h1>
-                        :
-                        null
-                    
-                    }
-                </div>
+                {this.props.currentMonster.hp > 0 &&
+                    <Link to='/UltimateRPG/hero/Map'>
+                        <button>Run!</button>
+                    </Link>
+                }
+                
+                
             </div>
-            {this.props.currentMonster.hp > 0 &&
-                <Link to='/UltimateRPG/hero/Map'>
-                    <button>Run!</button>
-                </Link>
-            }
-            
-            
-        </div>
         );
   }
 }
