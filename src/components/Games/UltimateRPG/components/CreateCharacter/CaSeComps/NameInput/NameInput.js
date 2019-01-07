77a7b7d6be936e12add300bac41e2064af4a5dd6
Spencer Smith <stix1919@hotmail.com>
2019-01-07 10:39:01 -0800

    date and name entry work. just need to send to database and create charm functionality

--- src/components/Games/UltimateRPG/components/CreateCharacter/CaSeComps/NameInput/NameInput.js
+++ src/components/Games/UltimateRPG/components/CreateCharacter/CaSeComps/NameInput/NameInput.js
@@ -10,23 +10,37 @@ class NameInput extends Component {
     constructor(props){
         super(props)
         this.state = {
-            name: '',
+            firstName: '',
+            lastName: '',
             birthDate: ''
         }
         this.changeHandler = this.changeHandler.bind(this)
+        this.dateHandler = this.dateHandler.bind(this)
     }
 
     changeHandler(input) {
         this.setState({[input.target.name]: input.target.value})
     }
 
-    render() {
+    dateHandler(input){
+        let date = input.target.value
+        console.log(date.length)
+        if(date.length === 4){
+            date += '-'
+        } else if (date.length === 7){
+            date += '-'
+        }
+        this.setState({[input.target.name]: date})
+    }
 
+    render() {
+        console.log(this.state)
         return (
             <div>
                 <h1>{this.state.name} {this.state.birthDate}</h1>
-                <input name='name' placeholder='enter name'/>
-                <input name='birthDate' placeholder='yyyy-mm-dd'/>
+                <input onChange={this.changeHandler} name='firstName' placeholder='First name'/>
+                <input onChange={this.changeHandler} name='lastName' placeholder='Last name'/>
+                <input type='date' onChange={this.dateHandler} name='birthDate' placeholder='yyyy-mm-dd' value={this.state.birthDate}/>
             </div>
         )
     }
