ef2e2f22790fcccfc615656ce78f9912fc07735e
Spencer Smith <stix1919@hotmail.com>
2019-01-09 09:07:48 -0800

    charm is being created. just need to send info to server

--- src/components/Games/UltimateRPG/components/CreateCharacter/CaSeComps/NameInput/NameInput.js
+++ src/components/Games/UltimateRPG/components/CreateCharacter/CaSeComps/NameInput/NameInput.js
@@ -6,8 +6,6 @@ import {connect} from 'react-redux'
 
 import './NameInput.css'
 
-require('dotenv').config();
-
 class NameInput extends Component {
     constructor(props){
         super(props)
@@ -30,6 +28,36 @@ class NameInput extends Component {
     }
     sendCharmInfo(){
         let splDate = this.state.birthDate.split('-')
+
+        let zodiac = [
+            {index: 0, name: 'dragon', stats: [20, 0, 0, 0]},
+            {index: 1, name: 'goblin', stats: [0, 10, 10, 0]},
+            {index: 2, name: 'slime', stats: [0, 0, 20, 0]},
+            {index: 3, name: 'griffon', stats: [10, 10, 0, 0]},
+            {index: 4, name: 'fairy', stats: [5, 5, 5, 5]},
+            {index: 5, name: 'angel', stats: [0, 0, 0, 20]},
+            {index: 6, name: 'wolf', stats: [0, 20, 0, 0]},
+            {index: 7, name: 'mermaid', stats: [0, 0, 10, 10]},
+            {index: 8, name: 'giant', stats: [10, 0 ,10 , 0]},
+            {index: 9, name: 'ghost', stats: [5, 5, 5, 5]},
+            {index: 10, name: 'gorgon', stats: [0, 10, 0, 10]},
+            {index: 11, name: 'demon', stats: [10, 0, 0, 10]}
+          ]
+          
+        let months=[
+            {name: 'January', start: 0, end: 1},
+            {name: 'Febuary', start: 1, end: 2},
+            {name: 'March', start: 2, end: 3},
+            {name: 'April', start: 3, end: 0},
+            {name: 'May', start: 1, end: 3},
+            {name: 'June', start: 2, end: 0},
+            {name: 'July', start: 3, end: 1},
+            {name: 'August', start: 0, end: 2},
+            {name: 'September', start: 2, end: 1},
+            {name: 'October', start: 3, end: 2},
+            {name: 'November', start: 0, end: 3},
+            {name: 'December', start: 1, end: 0},
+          ]
 
         function conversion(firstName, lastName){
             let key = {
@@ -106,7 +134,7 @@ class NameInput extends Component {
           
           
           function growthRate(month, day, year) {
-            let zodiacSign = process.env.REACT_APP_ZODIAC[year % 12]
+            let zodiacSign = zodiac[year % 12]
             console.log(zodiacSign)
             let growthKey = [
               null, 
@@ -148,7 +176,7 @@ class NameInput extends Component {
               }
               //added bonuses from year born
           
-              let monthBonus = process.env.REACT_APP_MONTHS[month - 1],
+              let monthBonus = months[month - 1],
                 dayDiff = 0,
                 side = null,
                 otherSide = null
@@ -187,7 +215,6 @@ class NameInput extends Component {
     }
 
     render() {
-        console.log(process.env)
         return (
             <div>
                 <h1>{this.state.name} {this.state.birthDate}</h1>
