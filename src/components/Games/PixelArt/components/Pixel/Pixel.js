121079b2d07913ef52e770b7f286660ba7d1dd00
Spencer Smith <stix1919@hotmail.com>
2019-03-21 12:32:25 -0600

    Some other stuff

--- src/components/Games/PixelArt/components/Pixel/Pixel.js
+++ src/components/Games/PixelArt/components/Pixel/Pixel.js
@@ -9,12 +9,12 @@ class Pixel extends Component {
             <div style={{border: this.props.border ? 'solid black .5px' : 'solid black 0' ,width: this.props.pixSize + 'px', height: this.props.pixSize + 'px', backgroundColor: this.props.pixel.color, opacity: this.props.pixel.opacity}}
             onClick={() => this.props.chooseColor(this.props.color, this.props.opacity, this.props.index, this.props.allArr)}
             onMouseEnter={e => {
-                if(e.buttons === 1 && this.props.color !== this.props.pixel.color){
+                if(e.buttons === 1 && this.props.color !== this.props.pixel.color || e.buttons === 1 && this.props.pixel.opacity !== this.props.opacity){
                     this.props.chooseColor(this.props.color, this.props.opacity, this.props.index, this.props.allArr)
                 }
             }}
             onMouseLeave={e => {
-                if(e.buttons === 1 && this.props.pixel.color !== this.props.color){
+                if(e.buttons === 1 && this.props.pixel.color !== this.props.color || e.buttons === 1 && this.props.pixel.opacity !== this.props.opacity){
                     this.props.chooseColor(this.props.color, this.props.opacity, this.props.index, this.props.allArr)
                 }
             }}
